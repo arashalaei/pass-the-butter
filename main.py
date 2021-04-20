e28be9f5660d8b4764ff502f71bb61bafb32c50e
arashalaei <arashalaei22@gmail.com>
2021-04-20 16:56:39 +0430

    First commit.

---
+++ main.py
@@ -0,0 +1,7 @@
+## In this project we want to implement some search algorithms like: IDS, Bidirectional BFS and A*.
+""" 
+    Contributors: Arash Alaei <arashalaei22@gmail.com>, 
+                  Elahe Sadeghi
+"""
+if __name__ == '__main__':
+    print("Only for the first commit")
