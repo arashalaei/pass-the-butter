5a6566dfe8a9ae76d8876418a22f002c18f4d2d5
arashalaei <arashalaei22@gmail.com>
2021-04-23 15:17:42 +0430

    Graph implementation based on Adjacency matrix was added.

--- Graph.py
+++ Graph.py
@@ -3,16 +3,24 @@
     @since Wednesday, April 21, 2021. 
 """
 
+import numpy as np
 from Vertex import Vertex
 
 class Graph:
-    
-    data_store:dict     # To store graph data.
-    vertices_number:int # Number of graph vertices.
-    edges_number:int    # Number of graph edges.
 
-    def __init__(self) -> None:
+    __implementation:str                # Specifies the type of graph implementation.
+    __size:int                          # Adjacency matrix size.
+    data_store:dict                     # To store graph data.
+    adjacency_matrix:np.ndarray         # Adjacency matrix.
+    vertices_number:int                 # Number of graph vertices.
+    edges_number:int                    # Number of graph edges.
+
+
+    def __init__(self, implementation:str = 'list', size:int = 100) -> None:
+        self.__implementation:str = implementation
+        self.__size:int = size
         self.data_store:dict = {}
+        self.adjacency_matrix:np.ndarray = np.zeros((self.__size, self.__size))
         self.vertices_number:int = 0
         self.edges_number:int = 0
 
@@ -25,24 +33,39 @@ class Graph:
     """
     def add_edge(self, src:int, dest:int) -> None:
 
-        added:bool = False # To determine an edge has been added or not.
+        if self.__implementation == 'list': # implementation == adjacency list
 
-        if src not in self.data_store:
-            self.data_store[src] = Vertex(src)
-            self.vertices_number += 1 # Increase the number of vertices.
+            added:bool = False # To determine an edge has been added or not.
+            
+            if src not in self.data_store:
+                self.data_store[src] = Vertex(src)
+                self.vertices_number += 1 # Increase the number of vertices.
 
-        if dest not in self.data_store:
-            self.data_store[dest] = Vertex(dest)
-            self.vertices_number += 1 # Increase the number of vertices.
+            if dest not in self.data_store:
+                self.data_store[dest] = Vertex(dest)
+                self.vertices_number += 1 # Increase the number of vertices.
 
-        if self.data_store[dest] not in self.data_store[src].adjacent_vertices:
-            self.data_store[src].adjacent_vertices.append(self.data_store[dest])
-            added = True
+            if self.data_store[dest] not in self.data_store[src].adjacent_vertices:
+                self.data_store[src].adjacent_vertices.append(self.data_store[dest])
+                added = True
 
-        if self.data_store[src] not in self.data_store[dest].adjacent_vertices:
-            self.data_store[dest].adjacent_vertices.append(self.data_store[src])
-            added = True            
+            if self.data_store[src] not in self.data_store[dest].adjacent_vertices:
+                self.data_store[dest].adjacent_vertices.append(self.data_store[src])
+                added = True            
 
-        if added:
-            self.edges_number += 1 # Increase the number of edges.
-    
+            if added:
+                self.edges_number += 1 # Increase the number of edges.
+
+        elif self.__implementation == 'matrix': # implementation == adjacency matrix
+            if src not in self.data_store:
+                self.data_store[src] = self.vertices_number
+                self.vertices_number += 1 # Increase the number of vertices.
+            
+            if dest not in self.data_store:
+                self.data_store[dest] = self.vertices_number
+                self.vertices_number += 1 # Increase the number of vertices.
+
+            if self.adjacency_matrix[self.data_store[src]][self.data_store[dest]] == 0:
+                self.adjacency_matrix[self.data_store[src]][self.data_store[dest]] = 1
+                self.adjacency_matrix[self.data_store[dest]][self.data_store[src]] = 1
+                self.edges_number += 1 # Increase the number of edges.
--- main.py
+++ main.py
@@ -7,8 +7,8 @@
 from Graph import Graph
 
 if __name__ == '__main__':
-    ## only for testing ##
-    g:Graph = Graph()
+    ## only for testing
+    g:Graph = Graph() # Implementation based on Adjacency list.
     
     g.add_edge(0, 1)
     g.add_edge(0, 2)
@@ -19,5 +19,20 @@ if __name__ == '__main__':
     g.add_edge(2, 3)
     g.add_edge(2, 4)
 
-    print(g.vertices_number)
-    print(g.edges_number)
+    print(" *** Adjacency list *** ")
+    print(f"Number of graph vertices: {g.vertices_number}")
+    print(f"Number of graph edges: {g.edges_number}")
+    print("##############################################")
+    print()
+
+    g2:Graph = Graph('matrix') # Implementation based on Adjacency matrix.
+    g2.add_edge(0,1)
+    g2.add_edge(1,0)
+    g2.add_edge(1,2)
+    g2.add_edge(1,2)
+    g2.add_edge(2,1)
+
+    print(" *** Adjacency matrix *** ")
+    print(g2.adjacency_matrix)
+    print(f"Number of graph vertices: {g2.vertices_number}")
+    print(f"Number of graph edges: {g2.edges_number}")
